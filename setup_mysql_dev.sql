d4a00ecd754c42daafdd66fdd09d919b45ccb694
ExtranoDev <jeomade19@gmail.com>
2023-01-20 17:53:31 +0000

    creates database and user

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,12 @@
---Script prepares a MySQL server
---Create a new database
+-- Script prepares a MySQL server
+-- Create a new database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---Create a new USER
+-- Create a new USER
+SET GLOBAL validate_password.policy = 0;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
---grant priviledges to new user
+-- grant priviledges to new user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
---grant select privileges on performance_schema
+-- grant select privileges on performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
---
